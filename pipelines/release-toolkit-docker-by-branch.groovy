10ceb5be63ddc3788147c3e6fb3e3dda0e64b482
dianqihanwangzi <dianqihanwangzi@gmail.com>
2021-07-19 19:44:51 +0800

    remove cron trgger

--- pipelines/release-toolkit-docker-by-branch.groovy
+++ pipelines/release-toolkit-docker-by-branch.groovy
@@ -14,14 +14,6 @@ properties([
                         defaultValue: false,
                         name: 'FORCE_REBUILD'
                 )
-        ]),
-        pipelineTriggers([
-            parameterizedCron('''
-                H H(0-7)/4 * * * % GIT_BRANCH=release-4.0
-                Hs H(0-7)/4 * * * % GIT_BRANCH=release-5.0
-                H H(0-7)/4 * * * % GIT_BRANCH=release-5.1
-                H H(0-7)/4 * * * % GIT_BRANCH=master
-            ''')
         ])
 ])
 
