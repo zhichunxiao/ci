83ddbb920e3fbf3d049aa9a486782c9145c4791a
purelind <purelind@gmail.com>
2021-08-27 14:47:05 +0800

    tiem: lint output with junit-xml

--- atom-jobs/atom-lint.groovy
+++ atom-jobs/atom-lint.groovy
@@ -30,7 +30,7 @@ properties([
                         trim: true
                 ),
                 text(
-                        defaultValue: 'make',
+                        defaultValue: 'golangci-lint run  --out-format=junit-xml  --timeout=10m  --config ./.golangci.yml  -v ./... > golangci-lint-report.xml',
                         name: 'LINT_CMD',
                         trim: true
                 ),
@@ -38,7 +38,7 @@ properties([
 ])
 
 
-def lint_config_url = ""
+LINT_CONFIG_URL = "https://raw.githubusercontent.com/purelind/devops-config/purelind/tiem-init-golangci-lint/tiem/golangci.yaml"
 
 
 def run_with_pod(Closure body) {
@@ -55,8 +55,8 @@ def run_with_pod(Closure body) {
                     containerTemplate(
                             name: 'golang', alwaysPullImage: false,
                             image: "${pod_go_docker_image}", ttyEnabled: true,
-                            resourceRequestCpu: '2000m', resourceRequestMemory: '2Gi',
-                            resourceLimitCpu: '4000m', resourceLimitMemory: "4Gi",
+                            resourceRequestCpu: '8000m', resourceRequestMemory: '8Gi',
+                            resourceLimitCpu: '20000m', resourceLimitMemory: "20Gi",
                             command: '/bin/sh -c', args: 'cat',
                             envVars: [containerEnvVar(key: 'GOMODCACHE', value: '/nfs/cache/mod'),
                                       containerEnvVar(key: 'GOPATH', value: '/go')],
@@ -83,35 +83,46 @@ def run_with_pod(Closure body) {
 try {
     run_with_pod {
         container("golang") {
-            def ws = pwd()
+            try {
+                def ws = pwd()
 
-            dir("${REPO}") {
-                stage("Download code from fileserver") {
-                    sh '''
+                dir("${REPO}") {
+                    stage("Download code from fileserver") {
+                        sh """
                     curl ${CACHE_CODE_FILESERVER_URL} | tar xz --strip-components=1
-                    '''
-                }
-
-                stage("Download lint conf") {
-                    sh '''
-                    curl -L -o .golangci.yml ${lint_config_url}
-                    '''
-                }
-
-                stage("Lint check") {
-                    dir("${ws}/${REPO}") {
-                        sh '''
-                        ${LINT_CMD}
-                        '''
+                    """
                     }
 
-                }
-            }
+                    stage("Download lint conf") {
+                        sh """
+                    curl -L -o .golangci.yml ${LINT_CONFIG_URL}
+                    """
+                    }
 
-            currentBuild.result = "SUCCESS"
+                    stage("Lint check") {
+                        sh """
+                        curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b ${ws}/bin v1.42.0
+                        """
+                        sh """
+                        export PATH=${ws}/bin:\$PATH
+                        golangci-lint version
+                        
+                        ${LINT_CMD}
+                        """
+                    }
+                }
+                currentBuild.result = "SUCCESS"
+
+            } catch (err) {
+                throw err
+            } finally {
+                junit(
+                    allowEmptyResults: true,
+                    testResults: "${REPO}/golangci-lint-report.xml"
+                )
+            }
         }
     }
-
 }
 
 catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
@@ -134,3 +145,5 @@ catch (Exception e) {
         currentBuild.result = "FAILURE"
         echo "${e}"
 }
+
+
