2d0d25ab1ba47edfd89f8cd7e40c0dd5ba5655b1
dianqihanwangzi <dianqihanwangzi@gmail.com>
2021-08-31 11:55:32 +0800

    update ut script

--- atom-jobs/atom-ut.groovy
+++ atom-jobs/atom-ut.groovy
@@ -1,144 +1,123 @@
-repo = "" // chanage to origin repo
-repoInfo = ghprbGhRepository.split("/")
-if (repoInfo.length == 2) {
-    repo = repoInfo[1]
-}
+properties([
+    parameters([
+        string(
+            defaultValue: 'tiem',
+            name: 'REPO',
+            trim: true
+        ),
+        string(
+            defaultValue: 'http://fileserver.pingcap.net/download/builds/pingcap/devops/cachecode/tiem/36c442509f86d58a69004059ba0ac3b74e4e4051/tiem.tar.gz',
+            name: 'CACHE_CODE_FILESERVER_URL',
+            trim: true
+        ),
+        string(
+            defaultValue: '36c442509f86d58a69004059ba0ac3b74e4e4051',
+            name: 'COMMIT_ID',
+            trim: true,
+        ),
+        text(
+            defaultValue: """
+go test -v ./... -coverprofile=cover.out |go-junit-report > test.xml
+gocov convert cover.out | gocov-xml > coverage.xml
+            """,
+            name: 'TEST_CMD',
+            trim: true
+        ),
+        string(
+            defaultValue: 'hub-new.pingcap.net/jenkins/centos7_golang-1.16',
+            name: 'TEST_ENV',
+            trim: true,
+        ),
+        string(
+            defaultValue: 'test.xml',
+            name: 'UT_REPORT_DIR',
+            trim: true,
+        ),
+        string(
+            defaultValue: 'coverage.xml',
+            name: 'COV_REPORT_DIR',
+            trim: true,
+        ),
+        string(
+            defaultValue: '50',
+            name: 'COVERAGE_RATE',
+            trim: true,
+        ),
+    ])
+])
 
-def configfile = "https://raw.githubusercontent.com/PingCAP-QE/devops-config/main/${repo}/verify.yaml"
-
-class Resource {
-   String cpu;
-   String memory;
-}
-
-class Env {
-   String image;
-   Resource limit;
-   Resource request;
-}
-
-class BuildConfig {
-   String shellScript;
-   String outputDir;
-   Env env;
-}
-
-class UnitTestConfig {
-   String shellScript;
-   String utReportDir;
-   String covReportDir;
-   String coverageRate;
-   Env env;
-}
-
-class LintConfig {
-   String shellScript;
-   String reportDir;
-}
-
-
-
-
-def getConfig(fileURL) {
-    sh "wget -qnc ${fileURL}"
-    configs = readYaml (file: "verify.yaml")
-    return configs
-}
-
-
-def parseBuildConfig(configs) {
-    def buildConfig = new BuildConfig()
-    buildConfig.outputDir = configs.build.outputDir.toString()
-    buildConfig.shellScript = configs.build.shellScript.toString()
-    return buildConfig
-}
-
-def parseUnitTestConfig(configs) {
-    def unitTestConfig = new UnitTestConfig()
-    unitTestConfig.utReportDir = configs.unitTest.utReportDir.toString()
-    unitTestConfig.covReportDir = configs.unitTest.covReportDir.toString()
-    unitTestConfig.shellScript = configs.unitTest.shellScript.toString()
-    unitTestConfig.coverageRate = configs.unitTest.coverageRate.toString()
-    return unitTestConfig
-}
-
-def parseLintConfig(configs) {
-    def lintConfig = new LintConfig()
-    lintConfig.reportDir = configs.lint.reportDir.toString()
-    lintConfig.shellScript = configs.lint.shellScript.toString()
-    return lintConfig
-}
-
-cacheCodeUrl = "${FILE_SERVER_URL}/download/builds/pingcap/devops/cachecode/${repo}/${ghprbActualCommit}/${repo}.tar.gz"
-
-def cacheCode() {
-    cacheCodeParams = [
-        string(name: 'REPO', value: repo),
-        string(name: 'COMMIT_ID', value: ghprbActualCommit),
-        string(name: 'PULL_ID', value: ghprbPullId),
-    ]
-    build(job: "cache-code", parameters: cacheCodeParams, wait: true)
-}
-
-def buildBinary(buildConfig) {
-    buildParams = [
-        string(name: 'REPO', value: repo),
-        string(name: 'CACHE_CODE_FILESERVER_URL', value: cacheCodeUrl),
-        string(name: 'COMMIT_ID', value: ghprbActualCommit),
-        text(name: 'BUILD_CMD', value: buildConfig.shellScript),
-        string(name: 'BUILD_ENV', value: "hub-new.pingcap.net/jenkins/centos7_golang-1.16"),
-        string(name: 'OUTPUT_DIR', value: "bin"),
-    ]
-    build(job: "atom-build", parameters: buildParams, wait: true)
-}
-
-def codeLint(lintConfig) {
-    lintParams = [
-        string(name: 'REPO', value: repo),
-        string(name: 'COMMIT_ID', value: ghprbActualCommit),
-        string(name: 'CACHE_CODE_FILESERVER_URL', value: cacheCodeUrl),
-        text(name: 'LINT_CMD', value: lintConfig.shellScript),
-        string(name: 'REPORT_DIR', value: lintConfig.reportDir),
-    ]
-    build(job: "atom-lint", parameters: lintParams, wait: true)
-}
-
-def unitTest(unitTestConfig) {
-    buildParams = [
-        string(name: 'REPO', value: repo),
-        string(name: 'COMMIT_ID', value: ghprbActualCommit),
-        string(name: 'CACHE_CODE_FILESERVER_URL', value: cacheCodeUrl),
-        text(name: 'TEST_CMD', value: unitTestConfig.shellScript),
-        string(name: 'UT_REPORT_DIR', value: unitTestConfig.utReportDir),
-        string(name: 'COV_REPORT_DIR', value: unitTestConfig.covReportDir),
-        string(name: 'COVERAGE_RATE', value: unitTestConfig.coverageRate),
-        string(name: 'TEST_ENV', value: "hub-new.pingcap.net/jenkins/centos7_golang-1.16"),
-    ]
-    build(job: "atom-ut", parameters: buildParams, wait: true)
+def run_with_pod(Closure body) {
+    def label = "atom-ut-atom-job"
+    def cloud = "kubernetes"
+    def namespace = "jenkins-tidb"
+    def pod_go_docker_image = 'hub-new.pingcap.net/jenkins/centos7_golang-1.16'
+    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
+    podTemplate(label: label,
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                            name: 'golang', alwaysPullImage: false,
+                            image: "${pod_go_docker_image}", ttyEnabled: true,
+                            resourceRequestCpu: '2000m', resourceRequestMemory: '2Gi',
+                            resourceLimitCpu: '4000m', resourceLimitMemory: "4Gi",
+                            command: '/bin/sh -c', args: 'cat',
+                            envVars: [containerEnvVar(key: 'GOMODCACHE', value: '/nfs/cache/mod'),
+                                      containerEnvVar(key: 'GOPATH', value: '/go')],
+                    ),
+                    containerTemplate(
+                            name: 'jnlp', image: "${jnlp_docker_image}", alwaysPullImage: false,
+                            resourceRequestCpu: '100m', resourceRequestMemory: '256Mi',
+                    ),
+            ],
+            volumes: [
+                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
+                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
+                    nfsVolume(mountPath: '/nfs/cache', serverAddress: '172.16.5.22',
+                            serverPath: '/mnt/ci.pingcap.net-nfs', readOnly: false),
+            ],
+    ) {
+        node(label) {
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
+            body()
+        }
+    }
 }
 
 
-node("${GO_BUILD_SLAVE}") {
+run_with_pod {
     container("golang") {
-        configs = getConfig(configfile)
-        buildConfig = parseBuildConfig(configs)
-        unitTestConfig = parseUnitTestConfig(configs)
-        lintConfig = parseLintConfig(configs)
-        stage("get code") {
-            cacheCode()
-        }
-        jobs = [:]
-        jobs["build"] = {
-            buildBinary(buildConfig)
-        }
-        jobs["lint"] = {
-            codeLint(lintConfig)
-        }
-        jobs["uint-test"] = {
-            unitTest(unitTestConfig)
-        }
-        stage("verify") {
-            parallel jobs
+        try {
+            def ws = pwd()
+
+            stage("Download binary from fileserver") {
+                sh '''
+                    wget -q -c ${CACHE_CODE_FILESERVER_URL} -O - | tar -xz
+                '''
+            }
+
+            stage("Test") {
+                dir("${ws}/${REPO}") {
+                    sh '''
+                        go get -u github.com/jstemmer/go-junit-report
+                        go get github.com/axw/gocov/gocov
+                        go get github.com/AlekSi/gocov-xml
+                    '''
+                    sh TEST_CMD
+                }
+            }
+        }  
+        catch (err) {
+            throw err
+        } finally {
+            junit testResults: "${REPO}/${UT_REPORT_DIR}"
+            if (currentBuild.result == 'UNSTABLE') {
+                currentBuild.result = 'FAILURE'
+            }
+            echo currentBuild.result
+            cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: "${REPO}/${COV_REPORT_DIR}", lineCoverageTargets: "${COVERAGE_RATE}, ${COVERAGE_RATE}, ${COVERAGE_RATE}", maxNumberOfBuilds: 10, onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+            echo currentBuild.result
         }
     }
 }
