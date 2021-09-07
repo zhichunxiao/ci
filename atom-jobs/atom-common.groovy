5d5b359a5a7ecdcb5fdd6f78e0841645c78942d7
dianqihanwangzi <dianqihanwangzi@gmail.com>
2021-09-07 16:01:11 +0800

    add common task type

---
+++ atom-jobs/atom-common.groovy
@@ -0,0 +1,125 @@
+/*
+* input
+* @REPO(string: github repo name, Required)
+* @COMMIT_ID(string: commit id request id, Required)
+* @CACHE_CODE_FILESERVER_URL(string: cached code on fileserver, Required)
+* @COMMON_CMD(txt: common cmd to run, Required)
+*
+*
+* doc
+*/
+
+properties([
+        parameters([
+                string(
+                        defaultValue: '',
+                        name: 'REPO',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'COMMIT_ID',
+                        trim: true,
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'CACHE_CODE_FILESERVER_URL',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'IMAGE',
+                        trim: true
+                ),
+                text(
+                        defaultValue: 'echo success',
+                        name: 'COMMON_CMD'
+                ),
+        ])
+])
+
+
+def run_with_pod(Closure body) {
+    def label = "circle-complexity-atom-job"
+    def cloud = "kubernetes"
+    def namespace = "jenkins-tidb"
+    def pod_go_docker_image = "hub-new.pingcap.net/jenkins/centos7_golang-1.16"
+    if (IMAGE.length() > 2) {
+        pod_go_docker_image = IMAGE
+    }
+    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
+    podTemplate(label: label,
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                            name: 'golang', alwaysPullImage: false,
+                            image: "${pod_go_docker_image}", ttyEnabled: true,
+                            resourceRequestCpu: '2000m', resourceRequestMemory: '4Gi',
+                            resourceLimitCpu: '2000m', resourceLimitMemory: "4Gi",
+                            command: '/bin/sh -c', args: 'cat',
+                            envVars: [containerEnvVar(key: 'GOMODCACHE', value: '/nfs/cache/mod'),
+                                      containerEnvVar(key: 'GOPATH', value: '/go')],
+                    ),
+                    containerTemplate(
+                            name: 'jnlp', image: "${jnlp_docker_image}", alwaysPullImage: false,
+                            resourceRequestCpu: '100m', resourceRequestMemory: '256Mi',
+                    ),
+            ],
+            volumes: [
+                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
+                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
+                    nfsVolume(mountPath: '/nfs/cache', serverAddress: '172.16.5.22',
+                            serverPath: '/mnt/ci.pingcap.net-nfs', readOnly: false),
+            ],
+    ) {
+        node(label) {
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
+            body()
+        }
+    }
+}
+
+try {
+    run_with_pod {
+        container("golang") {
+            def ws = pwd()
+
+            dir("${REPO}") {
+                stage("Download code from fileserver") {
+                    sh """
+                    curl ${CACHE_CODE_FILESERVER_URL} | tar xz --strip-components=1
+                    """
+                }
+
+                stage("common task") {
+                    sh COMMON_CMD
+                }
+            }
+            currentBuild.result = "SUCCESS"
+
+        }
+    }
+}
+
+catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+    println e
+    // this ambiguous condition means a user probably aborted
+    currentBuild.result = "ABORTED"
+} catch (hudson.AbortException e) {
+    println e
+    // this ambiguous condition means during a shell step, user probably aborted
+    if (e.getMessage().contains('script returned exit code 143')) {
+        currentBuild.result = "ABORTED"
+    } else {
+        currentBuild.result = "FAILURE"
+    }
+} catch (InterruptedException e) {
+    println e
+    currentBuild.result = "ABORTED"
+}
+catch (Exception e) {
+    currentBuild.result = "FAILURE"
+    echo "${e}"
+}
