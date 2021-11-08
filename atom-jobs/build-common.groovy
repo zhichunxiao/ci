4121d1bd5cbae2aa840c4aec529124d02c34cd93
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-11-08 22:32:33 +0800

    add ng monitoring build

--- atom-jobs/build-common.groovy
+++ atom-jobs/build-common.groovy
@@ -382,6 +382,22 @@ mkdir -p ${TARGET}/bin
 cp bin/* ${TARGET}/bin/   
 """
 
+buildsh["ng-monitoring"] = """
+if [ ${RELEASE_TAG}x != ''x ];then
+    for a in \$(git tag --contains ${GIT_HASH}); do echo \$a && git tag -d \$a;done
+    git tag -f ${RELEASE_TAG} ${GIT_HASH}
+    git branch -D refs/tags/${RELEASE_TAG} || true
+    git checkout -b refs/tags/${RELEASE_TAG}
+fi;
+if [[ ${ARCH} == 'arm64' ||  ${OS} == 'darwin' ]]; then
+    export PATH=${binPath}
+fi;
+make
+rm -rf ${TARGET}
+mkdir -p ${TARGET}/bin    
+cp bin/* ${TARGET}/bin/  
+"""
+
 buildsh["tidb-enterprise-tools"] = """
 if [ ${RELEASE_TAG}x != ''x ];then
     for a in \$(git tag --contains ${GIT_HASH}); do echo \$a && git tag -d \$a;done
