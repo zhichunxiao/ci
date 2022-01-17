b20c81ffe6e40177674cc1f26137946d02378c1b
zhouqiang <zhouqiang.cl@gmail.com>
2022-01-17 17:48:50 +0800

    compatibility to tiflash (#109)

--- atom-jobs/docker-common.groovy
+++ atom-jobs/docker-common.groovy
@@ -92,6 +92,18 @@ else
 fi
 """
 
+buildImgagesh["tiflash"] = """
+curl -o Dockerfile ${DOCKERFILE}
+if [[ ${RELEASE_TAG} == "" ]]; then
+    # No release tag, the image may be used in testings
+    docker build -t ${imagePlaceHolder} . --build-arg INSTALL_MYSQL=1
+else
+    # Release tag provided, do not install test utils
+    docker build -t ${imagePlaceHolder} . --build-arg INSTALL_MYSQL=0
+fi
+"""
+
+
 buildImgagesh["monitoring"] = """
 docker build -t ${imagePlaceHolder} .
 """
