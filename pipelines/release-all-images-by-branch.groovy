880f7ffdec2f731bc900b7daa7b0e1a4de9e0198
mahjonp <junpeng.man@gmail.com>
2022-01-18 18:56:05 +0800

    Update release-all-images-by-branch.groovy (#111)

--- pipelines/release-all-images-by-branch.groovy
+++ pipelines/release-all-images-by-branch.groovy
@@ -17,13 +17,13 @@ properties([
         ]),
         pipelineTriggers([
             parameterizedCron('''
-                H H(0-23)/4 * * * % GIT_BRANCH=release-4.0
-                H H(0-23)/4 * * * % GIT_BRANCH=release-5.0
-                H H(0-23)/4 * * * % GIT_BRANCH=release-5.1
-                H H(0-23)/4 * * * % GIT_BRANCH=release-5.2
-                H H(0-23)/4 * * * % GIT_BRANCH=release-5.3
-                H H(0-23)/4 * * * % GIT_BRANCH=release-5.4
-                H H(0-23)/4 * * * % GIT_BRANCH=master
+                # H H(0-23)/4 * * * % GIT_BRANCH=release-4.0
+                H H(0-23)/12 * * * % GIT_BRANCH=release-5.0
+                H H(0-23)/12 * * * % GIT_BRANCH=release-5.1
+                H H(0-23)/12 * * * % GIT_BRANCH=release-5.2
+                H H(0-23)/12 * * * % GIT_BRANCH=release-5.3
+                H H(0-23)/12 * * * % GIT_BRANCH=release-5.4
+                H H(0-23)/12 * * * % GIT_BRANCH=master
             ''')
         ])
 ])
