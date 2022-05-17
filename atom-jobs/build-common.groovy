bcb58f321be7c2a785bdc6c51cc9aaab4e5cd2e2
Lifu Wu <purelind@users.noreply.github.com>
2022-05-17 19:02:05 +0800

    go 1.18 use go1.18.2 (#212)

--- atom-jobs/build-common.groovy
+++ atom-jobs/build-common.groovy
@@ -144,7 +144,7 @@ def String needUpgradeGoVersion(String tag,String branch) {
 }
 
 def goBuildPod = "${GO1180_BUILD_SLAVE}"
-def GO_BIN_PATH = "/usr/local/go1.18/bin"
+def GO_BIN_PATH = "/usr/local/go1.18.2/bin"
 goVersion = needUpgradeGoVersion(params.RELEASE_TAG,params.TARGET_BRANCH)
 if ( goVersion == "go1.16" ) {
     goBuildPod = "${GO1160_BUILD_SLAVE}"
