ace43cb211c6436a217a3e592d90a1edc1e1ad66
heibaijian <hongmeng.lv@pingcap.com>
2022-05-19 19:12:18 +0800

    Nightlyfix0519 (#217)
    * debug build
    
    * complete
    
    * complete
    
    * complete dm
    
    * add try-catch
    
    * update repo
    
    * update local check
    
    * add notify
    
    * modify notify
    
    * update param
    
    * update boolean params
    
    * fix bug

--- pipelines/release-all-images-by-branch.groovy
+++ pipelines/release-all-images-by-branch.groovy
@@ -61,10 +61,11 @@ if (GIT_BRANCH.startsWith("release-")) {
 
 def get_sha(repo) {
     sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/get_hash_from_github.py > gethash.py"
-    if (repo == "ng-monitoring" && GIT_BRANCH=="master") {
-        GIT_BRANCH = "main"
+    def branch = "${GIT_BRANCH}"
+    if (repo == "ng-monitoring" && GIT_BRANCH == "master") {
+        branch = "main"
     }
-    return sh(returnStdout: true, script: "python gethash.py -repo=${repo} -version=${GIT_BRANCH} -s=${FILE_SERVER_URL}").trim()
+    return sh(returnStdout: true, script: "python gethash.py -repo=${repo} -version=${branch} -s=${FILE_SERVER_URL}").trim()
 }
 
 
@@ -598,21 +599,21 @@ retry(2) {
                         release_one_normal(product)
                     }
                 }
-            }
-            releaseReposMultiArch = ["tidb", "tikv", "pd", "br", "tidb-lightning", "ticdc", "dumpling", "tidb-binlog"]
-            if ("${GIT_BRANCH}" >= "release-5.3" || "${GIT_BRANCH}" == "master") {
-                releaseReposMultiArch = ["tidb", "tikv", "pd", "br", "tidb-lightning", "ticdc", "dumpling", "tidb-binlog", "dm", "ng-monitoring"]
-            }
-            for (item in releaseReposMultiArch) {
-                def String product = "${item}"
-                def String stageName = "${product}-multi-arch"
-                if (!params.NEED_MULTIARCH) {
-                    stageName = "${product}"
+                releaseReposMultiArch = ["tidb", "tikv", "pd", "br", "tidb-lightning", "ticdc", "dumpling", "tidb-binlog"]
+                if ("${GIT_BRANCH}" >= "release-5.3" || "${GIT_BRANCH}" == "master") {
+                    releaseReposMultiArch = ["tidb", "tikv", "pd", "br", "tidb-lightning", "ticdc", "dumpling", "tidb-binlog", "dm", "ng-monitoring"]
                 }
-                builds[stageName] = {
-                    release_one_normal(product)
-                    if (product != "ng-monitoring") {
-                        release_one_debug(product)
+                for (item in releaseReposMultiArch) {
+                    def String product = "${item}"
+                    def String stageName = "${product}-multi-arch"
+                    if (params.NEED_MULTIARCH == "false") {
+                        stageName = "${product}"
+                    }
+                    builds[stageName] = {
+                        release_one_normal(product)
+                        if (product != "ng-monitoring") {
+                            release_one_debug(product)
+                        }
                     }
                 }
                 failpointRepos = ["tidb", "pd", "tikv", "br", "tidb-lightning"]
@@ -624,8 +625,8 @@ retry(2) {
                 }
                 parallel builds
             }
+            currentBuild.result = "SUCCESS"
         }
-        currentBuild.result = "SUCCESS"
     } catch (Exception e) {
         currentBuild.result = "FAILURE"
         echo 'Waiting 5 minutes'
