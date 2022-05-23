3e131e783e3583c7f0c4e5b403a07b2473612390
heibaijian <hongmeng.lv@pingcap.com>
2022-05-23 21:48:51 +0800

    update manifest-multi-common for community rc (#219)
    * update manifest-multi-common for community rc
    
    * update param

--- atom-jobs/manifest-multiarch-common.groovy
+++ atom-jobs/manifest-multiarch-common.groovy
@@ -15,14 +15,19 @@ properties([
                         name: 'MULTI_ARCH_IMAGE',
                         trim: true,
                 ),
+                booleanParam(
+                        defaultValue: false,
+                        name: 'IF_ENTERPRISE'
+                ),
         ])
 ])
 
 
 node("delivery") {
     container("delivery") {
-        withCredentials([usernamePassword(credentialsId: 'harbor-pingcap', usernameVariable: 'harborUser', passwordVariable: 'harborPassword')]) {
-            sh """
+        stage("build multi-arch") {
+            withCredentials([usernamePassword(credentialsId: 'harbor-pingcap', usernameVariable: 'harborUser', passwordVariable: 'harborPassword')]) {
+                sh """
             docker login -u ${harborUser} -p ${harborPassword} hub.pingcap.net
             cat <<EOF > manifest.yaml
 image: ${MULTI_ARCH_IMAGE}
@@ -44,9 +49,25 @@ EOF
             chmod +x manifest-tool
             ./manifest-tool push from-spec manifest.yaml
             """
-                
+
             }
             archiveArtifacts artifacts: "manifest.yaml", fingerprint: true
+        }
+        if (IF_ENTERPRISE == "false") {
+            stage("sync community image to dockerhub") {
+                source_image = params.IF_ENTERPRISE
+                dest_image = source_image.replace("hub.pingcap.com/qa", "pingcap")
+                def default_params = [
+                        string(name: 'SOURCE_IMAGE', value: source_image),
+                        string(name: 'TARGET_IMAGE', value: dest_image),
+                ]
+                build(job: "jenkins-image-syncer",
+                        parameters: default_params,
+                        wait: true)
+
+            }
+        }
+
     }
     println "multi arch image: ${MULTI_ARCH_IMAGE}"
 }
