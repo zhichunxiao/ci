a482125b9c2edabf51ef7825db36eaedd844d57b
heibaijian <hongmeng.lv@pingcap.com>
2022-05-25 23:06:15 +0800

    refactor manifest (#226)
    * rename monitoring nightly image to tidb-monitor-initializer
    
    * enterprise image sync gcr
    
    * refactor manifest

--- atom-jobs/manifest-multiarch-common.groovy
+++ atom-jobs/manifest-multiarch-common.groovy
@@ -1,5 +1,3 @@
-import java.text.SimpleDateFormat
-
 properties([
         parameters([
                 string(
@@ -16,17 +14,9 @@ properties([
                         defaultValue: '',
                         name: 'MULTI_ARCH_IMAGE',
                         trim: true,
-                ),
-                booleanParam(
-                        defaultValue: false,
-                        name: 'IF_ENTERPRISE'
-                ),
+                )
         ])
 ])
-
-def date = new Date()
-sdf = new SimpleDateFormat("yyyyMMdd")
-day = sdf.format(date)
 
 node("delivery") {
     container("delivery") {
@@ -57,40 +47,6 @@ EOF
 
             }
             archiveArtifacts artifacts: "manifest.yaml", fingerprint: true
-        }
-        if (IF_ENTERPRISE == "false") {
-            stage("sync community image to dockerhub") {
-                source_image = params.MULTI_ARCH_IMAGE
-                dest_image = source_image.replace("hub.pingcap.net/qa", "pingcap")
-                def default_params = [
-                        string(name: 'SOURCE_IMAGE', value: source_image),
-                        string(name: 'TARGET_IMAGE', value: dest_image),
-                ]
-                build(job: "jenkins-image-syncer",
-                        parameters: default_params,
-                        wait: true)
-
-            }
-        } else {
-            stage("sync enterprise image to cloud") {
-                def source_image = params.MULTI_ARCH_IMAGE
-                // 命名规范：
-                //- vX.Y.Z-yyyymmdd，举例：v6.1.0-20220524
-                //- 特例：tidb-monitor-initializer 镜像格式要求，vX.Y.Z，举例：v6.1.0 （每次覆盖即可，这个问题是DBaaS上历史问题）
-                def dest_image = source_image.replace("hub.pingcap.net/qa", "gcr.io/pingcap-public/dbaas")
-                if (!dest_image.contains("tidb-monitor-initializer-enterprise")) {
-                    dest_image = dest_image + "-" + day
-                }
-
-                def default_params = [
-                        string(name: 'SOURCE_IMAGE', value: source_image),
-                        string(name: 'TARGET_IMAGE', value: dest_image),
-                ]
-                build(job: "jenkins-image-syncer",
-                        parameters: default_params,
-                        wait: true)
-
-            }
         }
 
     }
