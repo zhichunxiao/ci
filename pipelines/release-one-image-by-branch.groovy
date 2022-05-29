8e5d65900d86ca0e5d36d90676d88d090f5d5631
Lifu Wu <purelind@users.noreply.github.com>
2022-05-29 16:57:47 +0800

    use new cluster (#231)
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/release-one-image-by-branch.groovy
+++ pipelines/release-one-image-by-branch.groovy
@@ -93,9 +93,37 @@ def release_one(repo) {
         }
 }
 
+def run_with_pod(Closure body) {
+    def label = "${JOB_NAME}-${BUILD_NUMBER}"
+    def cloud = "kubernetes-ng"
+    def namespace = "jenkins-cd"
+    podTemplate(label: label,
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                        name: 'golang', alwaysPullImage: true,
+                        image: "hub.pingcap.net/jenkins/centos7_golang-1.18:latest", ttyEnabled: true,
+                        resourceRequestCpu: '200m', resourceRequestMemory: '1Gi',
+                        command: '/bin/sh -c', args: 'cat',
+                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
+                    )
+            ],
+            volumes: [
+                    emptyDirVolume(mountPath: '/tmp', memory: false),
+                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
+                    ],
+    ) {
+        node(label) {
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
+            body()
+        }
+    }
+}
 
 stage("build") {
-    node("${GO_BUILD_SLAVE}") {
+    run_with_pod {
         container("golang") {
             release_one(REPO)
         }
--- pipelines/tcms-daily-reports.groovy
+++ pipelines/tcms-daily-reports.groovy
@@ -24,11 +24,40 @@ stage("Wait for completion") {
     }
 }
 
+def run_with_pod(Closure body) {
+    def label = "${JOB_NAME}-${BUILD_NUMBER}"
+    def cloud = "kubernetes-ng"
+    def namespace = "jenkins-ti-pipeline"
+    podTemplate(label: label,
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                        name: 'golang', alwaysPullImage: true,
+                        image: "hub.pingcap.net/jenkins/centos7_golang-1.18:latest", ttyEnabled: true,
+                        resourceRequestCpu: '200m', resourceRequestMemory: '1Gi',
+                        command: '/bin/sh -c', args: 'cat',
+                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
+                    )
+            ],
+            volumes: [
+                    emptyDirVolume(mountPath: '/tmp', memory: false),
+                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
+                    ],
+    ) {
+        node(label) {
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
+            body()
+        }
+    }
+}
+
 def response = httpRequest tcmsHost + "api/v1/plans/branchs"
 def branches = readJSON text: response.content
 for (b in branches) {
     branch= b.toString()
-    node(GO_BUILD_SLAVE){
+    run_with_pod{
         container("golang"){
             stage("branch: "+ branch + " daily ci result") {
                 sh """
